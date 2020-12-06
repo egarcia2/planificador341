4fe0e1abfe5e392c1f0f63ca670f7a77d9415067
Jennifer <jmiranda@cs.wellesley.edu>
2020-12-05 23:04:05 -0500

    changes to SJF

--- SJF.java
+++ SJF.java
@@ -17,7 +17,46 @@ public class SJF{
     }
 
     public void run() {
+        this.sortJobs(); 
+        clock1.addTime(joblist.get(0).getArrivalTime());
+         
+        Job firstJob = jobList.get(0);
+        Job shortestJob = jobList.get(0);
+        int i = 0; 
+        double currentRuntime = firstJob.getRuntime();
         
+        while(firstJob.getArrivalTime() == jobList.get(i).getArrivalTime()) {
+            if (jobList.get(i).getRuntime() <= currentRuntime) {
+                currentRuntime = jobList.get(i).getRuntime(); 
+                shortestJob = jobList.get(i); 
+            }
+            i++; 
+        }
+        clock1.setTime(shortestJob.getArrivalTime());
+        System.out.println("Job 1" + " starting at time: " + clock1.getTime());
+        clock1.addTime(shortestJob.getRunTime());
+        jobList.remove(shortestJob); 
+        System.out.println("Job 1" + " finished at time: " + clock1.getTime());
+
+        shortestJob = jobList.get(0);
+        for(int i = 0; i< joblist.size(); i++) { 
+            Job jobi = joblist.get(i);
+            if (jobi.getArrivalTime() < clock1.getTime()) {
+                if (jobi.getRuntime() < shortestJob.getRunTime()){
+                    System.out.println("Job 2" + " starting at time: " + clock1.getTime());
+                }
+            }
+                    
+            double arriveTime = jobi.getArrivalTime();
+            if(arriveTime > clock1.getTime()) {
+                clock1.setTime(arriveTime);
+            }
+            System.out.println("Job " + (i+1) + " starting at time: " + clock1.getTime());
+            //simulate runtime
+            clock1.addTime(jobi.getRunTime());
+            System.out.println("Job " + (i+1) + " finished at time: " + clock1.getTime());
+            System.out.println();
+        }
     }
 
     public static void main (String[] args) {
@@ -28,25 +67,6 @@ public class SJF{
         jobArray.add(new Job(1, 3, false, 0));
         jobArray.add(new Job(5, 3, false, 0));
         jobArray.add(new Job(1, 10, false, 0));
-        
-        // call sort jobs 
-
-        // Clock clock1 = new Clock();
-
-        // //add the arrival time 
-        // clock1.addTime(jobArray[0].getArrivalTime());
-           
-        // for(int i = 0; i<4; i++) {
-        //     Job jobi = jobArray[i];
-        //     double arriveTime = jobi.getArrivalTime();
-        //     if(arriveTime > clock1.getTime()) {
-        //         clock1.setTime(arriveTime);
-        //     }
-        //     System.out.println("Job " + (i+1) + " starting at time: " + clock1.getTime());
-        //     //simulate runtime
-        //     clock1.addTime(jobi.getRunTime());
-        //     System.out.println("Job " + (i+1) + " finished at time: " + clock1.getTime());
-        //     System.out.println();
-        // }
+     
     }
 }
